101e9c1e1ab0b7e90d364f890bcd4b953d7654c2
scarlettperry <si.perry15@gmail.com>
2018-08-11 11:40:12 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
+  resources :students, only: [:index]
 
   # This show route can be refactored into the above resources method call, like so:
   # resources :students, only: [:index, :show]
   # However, for the sake of this lab and seeing how you can pass params through
   # the route, we'll keep it explicit for now and refactor later.
-  get "students/:id", to: "students#show"
+
+  #show action
+  get "students/:id", to: "students#show", as: :student
+
+  #edit action
+  get "students/:id/activate", to: "students#activate", as: :edit_student
+
+  #update action
+  patch "students/:id", to: "students#update"
 end
