aafef1035a2886045eae1277d43b8306a5e3674e
github username <github email address>
2018-08-13 16:42:25 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+student_1 =  Student.create(first_name: "Tyrell", last_name: "Gordon")
+student_2 =  Student.create(first_name: "Jack", last_name: "Stew")
+student_3 =  Student.create(first_name: "Jill", last_name: "Gillllian")
+student_4 =  Student.create(first_name: "Funkl", last_name: "Goon")
+student_5 =  Student.create(first_name: "Tell", last_name: "Don")
